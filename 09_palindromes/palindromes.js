2b28a9455c544ee6680f7b169e3af371694cc898
Chris Hodgetts <64872929+ChristopherHodgetts@users.noreply.github.com>
2024-07-09 13:38:02 +0100

    09 create a list of chars, take a string which is cleaned, reversed and then compared to the string, remove skips from test file

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const chars = "abcdefghijklmnopqrstuvwxyz0123456789"
 
+    const cleanedString = string
+        .toLowerCase()
+        .split('')
+        .filter((char) => chars.includes(char))
+        .join('');
+
+    const reversedString = cleanedString.split('').reverse().join('')
+
+    return cleanedString === reversedString
 };
 
 // Do not edit below this line
